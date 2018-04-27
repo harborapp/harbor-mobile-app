f9d73f8302a135f1ae119d731cd2773385be19fb
Srishan Bhattarai <srishanbhattarai@gmail.com>
2018-04-27 23:53:10 +0545

    chore: Use the styles-merge package

--- app/components/Container/index.js
+++ app/components/Container/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { View } from 'react-native'
-
-import styleNames from '../../utils/styleNames'
+import stylesmerge from 'styles-merge'
 
 /**
  * Container is a generic element to wrap children
@@ -11,7 +10,7 @@ import styleNames from '../../utils/styleNames'
  * @returns {jsx}
  */
 const Container = ({ children, centerChildren, flex, style, ...props }) => {
-  const styles = styleNames({ flex })
+  const styles = stylesmerge({ flex })
     .ifThen(style !== undefined, style)
     .ifThen(centerChildren === true, { alignItems: 'center', justifyContent: 'center' })
     .eval()
