a9ca6ea3b96e7de8ec8083d98081dfe33519ec6b
Srishan Bhattarai <srishanbhattarai@gmail.com>
2018-04-27 10:44:10 +0545

    feat: Add custom FormattedMessage component

--- app/components/FormattedMessage.js
+++ app/components/FormattedMessage.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import { Text } from 'react-native'
-import { injectIntl } from 'react-intl'
+import PropTypes from 'prop-types'
+import { injectIntl, intlShape } from 'react-intl'
 
 /**
- * The FormattedMessage component is equivalen't to the
- * FormattedMessage that react-intl provides except
- * that it works on React Native without issue.
+ * The FormattedMessage component is equivalent to the
+ * FormattedMessage component that react-intl
+ * provides except that it works on
+ * React Native without issue.
  *
  * @returns {jsx}
  */
@@ -17,5 +20,12 @@ const Message = ({
 }) => (
   <Text {...props}>{intl.formatMessage({id, defaultMessage}, values)}</Text>
 )
+
+Message.propTypes = {
+  intl: intlShape.isRequired,
+  id: PropTypes.string.isRequired,
+  defaultMessage: PropTypes.string,
+  values: PropTypes.object
+}
 
 export default injectIntl(Message)
---
+++ app/components/tests/FormattedMessage.test.js
@@ -0,0 +1,24 @@
+/* eslint-env jest */
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import { IntlProvider } from 'react-intl'
+
+import FormattedMessage from '../FormattedMessage'
+
+describe('FormattedMessage', () => {
+  it('should render', () => {
+    const props = {
+      id: 'greeting',
+      defaultMessage: 'Welcome to Harbor'
+    }
+
+    const tree = renderer.create(
+      <IntlProvider locale='en'>
+        <FormattedMessage {...props} />
+      </IntlProvider>
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+})
