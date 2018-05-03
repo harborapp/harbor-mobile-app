620e1bedd20ebad892350029e5cd932e69baad74
Srishan Bhattarai <srishanbhattarai@gmail.com>
2018-05-03 23:45:56 +0545

    feat: Preliminary login components

---
+++ app/components/GoogleLogin/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    width: '100%',
+    padding: 20,
+    backgroundColor: 'white' // TODO
+  }
+})
