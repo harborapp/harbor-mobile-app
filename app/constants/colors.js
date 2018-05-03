411e75e5ac05accc4dee11627e6515d5739df619
Srishan Bhattarai <srishanbhattarai@gmail.com>
2018-05-04 00:39:13 +0545

    feat: Add more colors

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,4 +1,6 @@
+export const WHITE = '#FFFFFF'
+export const BLACK = '#000000'
 export const GRADIENT_LOW = '#00D1FF'
 export const GRADIENT_HIGH = '#006DFF'
-export const WHITE = '#FFFFFF'
 export const DIMMED_BLUE = '#00D1FF'
+export const GOOGLE_ORANGE = '#DD4B39'
