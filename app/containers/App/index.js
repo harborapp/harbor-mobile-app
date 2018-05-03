3c4f984329f26452cfbf6c5bc549c5515b30d68e
Srishan Bhattarai <srishanbhattarai@gmail.com>
2018-05-03 23:45:29 +0545

    chore: Remove react router for now

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { NativeRouter } from 'react-router-native'
 
 import LoginPage from '../LoginPage'
 import Container from '../../components/Container'
 
 const App = () => (
-  <NativeRouter>
-    <Container>
-      <LoginPage />
-    </Container>
-  </NativeRouter>
+  <Container>
+    <LoginPage />
+  </Container>
 )
 
-export default connect()(App)
+export default App
